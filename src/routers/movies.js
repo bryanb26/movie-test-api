12152bd6dc9bfc91a0ef8114a67471fa6bf910ee
bryanb26 <50746711+bryanb26@users.noreply.github.com>
2020-01-10 11:44:08 -0500

    Day 4

--- src/routers/movies.js
+++ src/routers/movies.js
@@ -12,6 +12,24 @@ router.post("/movies", async (req, res) => {
     }
 });
 
+/* router.get("/movies", async (req, res) => {
+    try {
+        let movies = await Movie.find({});
+        res.send(movies);
+    } catch (error) {
+        res.status(500).send(error); 
+    }
+}) */
+
+router.get("/movies", async (req, res) => {
+    try {
+        let movies = await Movie.find({year: '2019'});
+        res.send(movies);
+    } catch (error) {
+        res.status(500).send(error); 
+    }
+})
+
 
 router.get("/movies/:id", async (req, res) => {
     try {
@@ -21,4 +39,26 @@ router.get("/movies/:id", async (req, res) => {
         res.status(500).send(error); 
     }
 })
+
+router.delete("/movies/:id", async (req,res) => {
+    try {
+        const movie = await Movie.findByIdAndDelete(req.params.id);
+        res.send(movie);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.patch("/movies/:id", async (req,res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["genre"];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update)
+    );
+    try {
+       const movie = await Movie.findByIdAndUpdate(req.params.id, req.body);
+        res.send(movie);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
 module.exports = router;
